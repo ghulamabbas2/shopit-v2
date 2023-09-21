8d3f1be340b34529551a53abfe2b478d939c93b3
Ghulam Abbas <ghulamabbas258@gmail.com>
2023-09-21 12:35:52 +0500

    fixed bug

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,6 +2,6 @@ import express from "express";
 import { getProducts } from "../controllers/productControllers.js";
 const router = express.Router();
 
-router.get("/products").use(getProducts);
+router.route("/products").get(getProducts);
 
 export default router;
