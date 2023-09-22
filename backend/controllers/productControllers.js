f47342d8abb3ba28ef4e6b9c5a893b5df3fb93f1
Ghulam Abbas <ghulamabbas258@gmail.com>
2023-09-22 12:01:27 +0500

    backend search, filters & pagination

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,12 +1,22 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import Product from "../models/product.js";
+import APIFilters from "../utils/apiFilters.js";
 import ErrorHandler from "../utils/errorHandler.js";
 
 // Create new Product   =>  /api/v1/products
 export const getProducts = catchAsyncErrors(async (req, res) => {
-  const products = await Product.find();
+  const resPerPage = 4;
+  const apiFilters = new APIFilters(Product, req.query).search().filters();
+
+  let products = await apiFilters.query;
+  let filteredProductsCount = products.length;
+
+  apiFilters.pagination(resPerPage);
+  products = await apiFilters.query.clone();
 
   res.status(200).json({
+    resPerPage,
+    filteredProductsCount,
     products,
   });
 });
---
+++ backend/utils/apiFilters.js
@@ -0,0 +1,45 @@
+class APIFilters {
+  constructor(query, queryStr) {
+    this.query = query;
+    this.queryStr = queryStr;
+  }
+
+  search() {
+    const keyword = this.queryStr.keyword
+      ? {
+          name: {
+            $regex: this.queryStr.keyword,
+            $options: "i",
+          },
+        }
+      : {};
+
+    this.query = this.query.find({ ...keyword });
+    return this;
+  }
+
+  filters() {
+    const queryCopy = { ...this.queryStr };
+
+    // Fields to remove
+    const fieldsToRemove = ["keyword", "page"];
+    fieldsToRemove.forEach((el) => delete queryCopy[el]);
+
+    // Advance filter for price, ratings etc
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+
+  pagination(resPerPage) {
+    const currentPage = Number(this.queryStr.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
+
+    this.query = this.query.limit(resPerPage).skip(skip);
+    return this;
+  }
+}
+
+export default APIFilters;
